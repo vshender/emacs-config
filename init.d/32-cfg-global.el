f318f4ec6a442103c131b320f7e3345566150544
Vadim Shender <vadim@shender.org>
2017-04-11 23:08:28 +0300

    Setup IDO

---
+++ init.d/32-cfg-global.el
@@ -0,0 +1,47 @@
+;; 32-cfg-global.el --- setting up global modes and core plugins  -*- lexical-binding: t -*-
+
+;;; Code:
+
+(defun cfg:global-module-init ()
+  "Entry function of global module for the cfg init system."
+
+  (cfg:-setup-ido))
+
+
+(defun cfg:-setup-ido ()
+  "Setup Interactive Do."
+  (require 'ido)
+
+  (custom-set-variables
+   '(ido-everywhere t)
+
+   '(ido-enable-last-directory-history t)
+
+   '(ido-enable-flex-matching t)
+   '(ido-enable-prefix nil)
+   '(ido-enable-case nil)
+
+   '(ido-ignore-extensions t)
+
+   '(ido-create-new-buffer 'always)
+   '(ido-use-filename-at-point nil)
+   '(ido-auto-merge-work-directories-length -1))
+
+  (customize-set-variable 'ido-file-extensions-order '(".yml" ".yaml" ".retry"))
+  (add-to-list 'completion-ignored-extensions ".retry")
+
+  (ido-mode)
+
+  (cfg:install ido-ubiquitous
+    (require 'ido-ubiquitous)
+    (ido-ubiquitous-mode))
+
+  (cfg:install smex
+    (customize-set-variable
+     'smex-save-file (locate-user-emacs-file "smex.hist"))
+    (smex-initialize)
+
+    (global-set-key (kbd "M-x") #'smex)
+    (global-set-key (kbd "M-X") #'smex-major-mode-commands)
+    ;; This is the old M-x.
+    (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)))
