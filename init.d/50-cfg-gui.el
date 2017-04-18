486587ea03573824b8cd1ddf4de8a271d9460d2f
Vadim Shender <vadim@shender.org>
2017-04-18 11:57:01 +0300

    Setup diminish for company, helm and yasnippet

--- init.d/50-cfg-gui.el
+++ init.d/50-cfg-gui.el
@@ -43,7 +43,10 @@
   "Setup minor modes diminishing."
   (cfg:install diminish
     (eval-after-load "abbrev" '(diminish 'abbrev-mode))
-    (eval-after-load "undo-tree" '(diminish 'undo-tree-mode))))
+    (eval-after-load "company" '(diminish 'company-mode))
+    (eval-after-load "helm" '(diminish 'helm-mode))
+    (eval-after-load "undo-tree" '(diminish 'undo-tree-mode))
+    (eval-after-load "yasnippet" '(diminish 'yas-minor-mode))))
 
 (defun cfg:-setup-color-theme ()
   "Setup Emacs color theme."
