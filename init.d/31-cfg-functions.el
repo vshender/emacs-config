08d325806e5c57228f9dc31874e3f81296938c7a
Vadim Shender <vadim@shender.org>
2017-04-23 11:13:46 +0300

    Setup Emacs for Emacs Lisp programming language

---
+++ init.d/31-cfg-functions.el
@@ -0,0 +1,16 @@
+;;; 31-cfg-functions.el --- some global function definitions  -*- lexical-binding: t -*-
+
+;;; Code:
+
+(defun cfg:start-or-switch-to (function buffer-name)
+  "Invoke FUNCTION if there is no buffer with BUFFER-NAME.
+Otherwise switch to the buffer named BUFFER-NAME.  Don't clobber
+the current buffer."
+  (if (not (get-buffer buffer-name))
+      (progn
+        (split-window-sensibly (selected-window))
+        (other-window 1)
+        (funcall function))
+    (switch-to-buffer-other-window buffer-name)))
+
+;;; 31-cfg-functions.el ends here
---
+++ modules/cfg-elisp.el
@@ -0,0 +1,33 @@
+;;; cfg-elisp.el --- setting up Emacs for Emacs Lisp programming language  -*- lexical-binding: t -*-
+
+;;; Code:
+
+;;;###autoload
+(defun cfg:elisp-module-init ()
+  "Entry function of elisp module for the cfg init system."
+  (cfg:install elisp-slime-nav
+    (cfg:with-local-autoloads
+      (dolist (hook '(emacs-lisp-mode-hook
+                      lisp-interaction-mode-hook
+                      ielm-mode-hook
+                      eshell-mode-hook))
+        (add-hook hook 'turn-on-elisp-slime-nav-mode))))
+
+  (defun cfg:visit-ielm ()
+    "Switch to default `ielm' buffer.
+Start `ielm' if it's not already running."
+    (interactive)
+    (cfg:start-or-switch-to 'ielm "*ielm*"))
+  (define-key emacs-lisp-mode-map (kbd "C-c C-z") 'cfg:visit-ielm)
+
+  (add-hook 'emacs-lisp-mode-hook #'cfg:elisp-hook))
+
+;;;###autoload (cfg:auto-module "\\.el\\'" elisp)
+
+
+(defun cfg:elisp-hook ()
+  "A hook that is called when emacs lisp mode is loaded."
+  (linum-mode t)
+  (yas-minor-mode t))
+
+;;; cfg-elisp.el ends here
