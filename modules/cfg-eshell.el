540dabbfff1eda80ea90010e4e5055a5defb799e
Vadim Shender <vadim@shender.org>
2017-04-23 11:23:11 +0300

    Setup eshell

---
+++ modules/cfg-eshell.el
@@ -0,0 +1,14 @@
+;;; cfg-eshell.el --- setting up Eshell  -*- lexical-binding: t -*-
+
+;;; Code:
+
+;;;###autoload
+(defun cfg:eshell-module-init ()
+  "Entry function of eshell module for the cfg init system."
+  (with-eval-after-load "eshell"
+    (customize-set-variable 'eshell-directory-name
+                            (expand-file-name "eshell" cfg:var-dir))))
+
+;;;###autoload (cfg:eshell-module-init)
+
+;;; cfg-eshell.el ends here
