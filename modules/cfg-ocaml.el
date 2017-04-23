f67f4232665c2df4b9dfbeeadccb542a876c2c9e
Vadim Shender <vadim@shender.org>
2017-04-23 11:10:37 +0300

    Add docstrings to ocaml module

--- modules/cfg-ocaml.el
+++ modules/cfg-ocaml.el
@@ -4,6 +4,7 @@
 
 ;;;###autoload
 (defun cfg:ocaml-module-init ()
+  "Entry function of ocaml module for the cfg init system."
   (cfg:install tuareg-mode
     (autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
     (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
@@ -32,6 +33,7 @@
 
 
 (defun cfg:-ocaml-hook ()
+  "A hook that is called when tuareg mode is loaded."
   (merlin-mode t)
   (utop-minor-mode t)
   (ocp-setup-indent)
