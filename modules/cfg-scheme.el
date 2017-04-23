94252360b91ab53769cb61d9a46874f63c4b2ad6
Vadim Shender <vadim@shender.org>
2017-04-23 21:54:11 +0300

    Setup Emacs for Scheme programming language

---
+++ modules/cfg-scheme.el
@@ -0,0 +1,29 @@
+;;; cfg-scheme.el --- setting up Emacs for Scheme programming language  -*- lexical-binding: t -*-
+
+;;; Code:
+
+;;;###autoload
+(defun cfg:scheme-module-init ()
+  "Entry function of scheme module for the cfg init system."
+  (cfg:install geiser
+    ;; Geiser elisp sources are in the "elisp" subdirectory.
+    (let ((default-directory (expand-file-name "elisp" default-directory)))
+      (cfg:with-local-autoloads
+        (with-eval-after-load 'geiser
+          (setq geiser-guile-load-init-file-p t))))))
+
+;;;###autoload (cfg:auto-module "\\.scm\\'" scheme)
+;;;###autoload (cfg:auto-module "\\.rkt\\'" scheme)
+
+
+(defun cfg:-scheme-hook ()
+  "A hook that is called when scheme mode is loaded."
+  (setq geiser-active-implementations '(racket guile))
+
+  (set (make-local-variable 'company-backends)
+       '(geiser-company-backend company-files))
+
+  (linum-mode t)
+  (yas-minor-mode t))
+
+;;; cfg-scheme.el ends here
