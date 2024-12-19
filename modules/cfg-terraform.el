2630a992c421bea8e16c6800c31a99c2e5cd3e23
Vadim Shender <vadim@shender.org>
2024-12-19 21:28:46 +0000

    Update terraform-mode configuration

--- modules/cfg-terraform.el
+++ modules/cfg-terraform.el
@@ -4,9 +4,17 @@
 
 ;;;###autoload
 (defun cfg:terraform-module-init ()
+  "Entry function of terraform module for the cfg init system."
   (cfg:install terraform-mode
-    (require 'terraform-mode)))
+    (require 'terraform-mode)
+
+    (add-hook 'terraform-mode-hook #'cfg:-terraform-hook)))
 
 ;;;###autoload (cfg:auto-module "\\.tf\\(vars\\)?\\'" terraform)
+
+
+(defun cfg:-terraform-hook ()
+  "A hook that is called when terraform mode is loaded."
+  (display-line-numbers-mode t))
 
 ;;; cfg-terraform.el ends here
