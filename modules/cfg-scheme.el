200e12633846dbf0d455ae8276172814dd093d24
Vadim Shender <vadim@shender.org>
2024-12-21 16:57:44 +0000

    Update Scheme programming language configuration

--- modules/cfg-scheme.el
+++ modules/cfg-scheme.el
@@ -5,14 +5,15 @@
 ;;;###autoload
 (defun cfg:scheme-module-init ()
   "Entry function of scheme module for the cfg init system."
-  (add-hook 'scheme-mode-hook #'cfg:-scheme-hook)
-
   (cfg:install geiser
     ;; Geiser elisp sources are in the "elisp" subdirectory.
     (let ((default-directory (expand-file-name "elisp" default-directory)))
-      (cfg:with-local-autoloads
-        (with-eval-after-load 'geiser
-          (setq geiser-guile-load-init-file-p t))))))
+      (cfg:with-local-autoloads)))
+
+  (cfg:install geiser-racket
+    (cfg:with-local-autoloads))
+
+  (add-hook 'scheme-mode-hook #'cfg:-scheme-hook))
 
 ;;;###autoload (cfg:auto-module "\\.scm\\'" scheme)
 ;;;###autoload (cfg:auto-module "\\.rkt\\'" scheme)
@@ -20,12 +21,6 @@
 
 (defun cfg:-scheme-hook ()
   "A hook that is called when scheme mode is loaded."
-  (setq geiser-active-implementations '(racket guile))
-
-  (setq-local company-backends
-              '(geiser-company-backend company-files))
-
-  (display-line-numbers-mode t)
-  (yas-minor-mode t))
+  (display-line-numbers-mode t))
 
 ;;; cfg-scheme.el ends here
