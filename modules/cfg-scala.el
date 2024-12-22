1567f8846957e9e6ef57bec44e34700f15a37580
Vadim Shender <vadim@shender.org>
2024-12-22 15:51:38 +0000

    Update Scala programming language configuration

--- modules/cfg-scala.el
+++ modules/cfg-scala.el
@@ -9,16 +9,12 @@
     (cfg:with-local-autoloads
       (add-hook 'scala-mode-hook #'cfg:-scala-hook)))
 
+  (cfg:install dap-mode
+    (cfg:with-local-autoloads))
+
   (cfg:install lsp-mode
     (cfg:with-local-autoloads
-      ;; TODO
-      (require 'lsp-modeline)
-      (require 'lsp-completion)
-
-      (add-hook 'scala-mode-hook #'lsp)
-      (add-hook 'lsp-mode-hook #'lsp-completion-mode)
-      (add-hook 'lsp-mode-hook #'lsp-lens-mode)
-      ))
+      (add-hook 'scala-mode-hook #'lsp)))
 
   (cfg:install lsp-metals
     (cfg:with-local-autoloads
@@ -26,22 +22,13 @@
       (setq lsp-metals-treeview-show-when-views-received nil)))
 
   (cfg:install lsp-ui
-    (require 'lsp-ui)
-    (add-hook 'scala-mode-hook #'lsp-ui-mode)
+    (cfg:with-local-autoloads))
+    ;; (add-hook 'scala-mode-hook #'lsp-ui-mode)
 
-    (setq lsp-ui-flycheck-enable t
-          lsp-ui-sideline-enable t))
-
-  (cfg:install posframe)
-
-  (cfg:install dap-mode
-    (cfg:with-local-autoloads
-      (add-hook 'lsp-mode #'dap-mode)
-      (add-hook 'lsp-mode #'dap-ui-mode)))
+  ;; (cfg:install posframe)
 
   (cfg:install sbt-mode
-    (cfg:with-local-autoloads))
-  )
+    (cfg:with-local-autoloads)))
 
 ;;;###autoload (cfg:auto-module "\\.scala\\'" scala)
 
@@ -49,8 +36,14 @@
 (defun cfg:-scala-hook ()
   "A hook that is called when scala mode is loaded."
 
-  ;; (lsp-completion-mode)
-  ;; (lsp-lens-mode)
+  (with-eval-after-load 'lsp-ui
+    (define-key lsp-ui-mode-map
+                [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+    (define-key lsp-ui-mode-map
+                [remap xref-find-references] #'lsp-ui-peek-find-references)
+
+    (setq lsp-ui-flycheck-enable t
+          lsp-ui-sideline-enable t))
 
   (display-line-numbers-mode t)
   (yas-minor-mode t))
