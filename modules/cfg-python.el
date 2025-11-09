402eea09910655c5c4f8ce5e5b116d278cb1fd72
Vadim Shender <vadim@shender.org>
2025-11-09 11:54:27 +0000

    Use .venv for Python projects

--- modules/cfg-python.el
+++ modules/cfg-python.el
@@ -5,9 +5,9 @@
 ;;;###autoload
 (defun cfg:python-module-init ()
   "Entry function of python module for the cfg init system."
-  (cfg:install python-environment
-    (require 'python-environment)
-    (setq python-environment-directory "~/.virtualenvs"))
+
+  (setq cfg:virtualenvs-path "~/.virtualenvs")
+  (setq cfg:default-virtualenv-path (expand-file-name "default" cfg:virtualenvs-path))
 
   (cfg:install company-jedi
     (cfg:with-local-autoloads)
@@ -21,6 +21,7 @@
 
 (defun cfg:-python-hook ()
   "A hook that is called when python mode is loaded."
+
   (cfg:setup-jedi)
   (cfg:setup-flycheck-for-python)
 
@@ -36,22 +37,14 @@
   (setq-local python-indent-def-block-scale 1))
 
 
-(defun cfg:project-venv-name ()
-  "Return a virtual environment name for the current python project."
-  (let* ((project-name (projectile-project-name))
-         (venv-path (expand-file-name project-name
-                                      python-environment-directory)))
-    (if (file-exists-p (expand-file-name "bin/activate" venv-path))
-        project-name
-      python-environment-default-root-name)))
-
-(defun cfg:project-venv-py-version ()
-  "Return a python version of the virtual environment for the current project."
-  (let ((venv-path (expand-file-name (cfg:project-venv-name)
-                                     python-environment-directory)))
-    (if (file-exists-p (expand-file-name "bin/python3" venv-path))
-        3
-      2)))
+;; (defun cfg:project-venv-name ()
+;;   "Return a virtual environment name for the current python project."
+;;   (let* ((project-name (projectile-project-name))
+;;          (venv-path (expand-file-name project-name
+;;                                       python-environment-directory)))
+;;     (if (file-exists-p (expand-file-name "bin/activate" venv-path))
+;;         project-name
+;;       python-environment-default-root-name)))
 
 (defun cfg:setup-jedi ()
   "Setup jedi."
@@ -61,7 +54,7 @@
 
   (setq-local jedi:environment-root
               (format "~/.emacs.d/.python-environments/py%d"
-                      (cfg:project-venv-py-version)))
+                      (cfg:python-project-py-version)))
 
   ;; Install jediepcserver if it's not installed yet.
   (let ((jediepcserver-path (expand-file-name "bin/jediepcserver"
@@ -72,7 +65,7 @@
 
       (let ((jedi:environment-virtualenv
              (list "virtualenv"
-                   (format "--python=python%d" (cfg:project-venv-py-version))
+                   (format "--python=python%d" (cfg:python-project-py-version))
                    "--quiet")))
         ;; `jedi:environment-virtualenv' is customized in order to specify
         ;; proper python version for the jedi virtual environment.
@@ -86,8 +79,7 @@
               (list (expand-file-name "bin/python" jedi:environment-root)
                     (expand-file-name "bin/jediepcserver" jedi:environment-root)))
   (setq-local jedi:server-args
-              (list "--virtual-env" (expand-file-name (cfg:project-venv-name)
-                                                      python-environment-directory)))
+              (list "--virtual-env" (cfg:python-project-venv-path)))
 
   (message "CFG: enable Jedi from %s venv"
            (file-name-nondirectory jedi:environment-root))
@@ -98,11 +90,26 @@
 
 (defun cfg:setup-flycheck-for-python ()
   "Setup flycheck for Python."
-  (let ((venv-path (expand-file-name (cfg:project-venv-name)
-                                     python-environment-directory)))
+  (let ((venv-path (cfg:python-project-venv-path)))
     (setq-local flycheck-python-flake8-executable
                 (expand-file-name "bin/flake8" venv-path))
     (setq-local flycheck-python-mypy-executable
                 (expand-file-name "bin/mypy" venv-path))))
+
+(defun cfg:python-project-venv-path ()
+  "Return the path to the current Python project.
+
+Use the default virtualenv if there is no virtualenv for the project."
+  (let ((project-venv-path (expand-file-name ".venv" (projectile-project-root))))
+    (if (file-exists-p (expand-file-name "bin/activate" project-venv-path))
+        project-venv-path
+      cfg:default-virtualenv-path)))
+
+(defun cfg:python-project-py-version ()
+  "Return a python version of the virtual environment for the current project."
+  (if (file-exists-p (expand-file-name "bin/python3"
+                                       (cfg:python-project-venv-path)))
+      3
+    2))
 
 ;;; cfg-python.el ends here
